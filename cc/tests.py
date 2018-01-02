e34dc586274ec4ed41cffc14a32854df6e6e0cfd
ddiben <ddiben@gmail.com>
2018-01-02 07:57:49 -0800

    updating the repository, currently working on the first test of BlogPosts

--- cc/tests.py
+++ cc/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import BlogPosts, Links
 
-# Create your tests here.
+class BlogPostsModelTest(TestCase):
+    def test_create_post(self):
+        #create a blogpost
+        
+        self.assertEquals(self.getblogPost(), 'First blog post')
+    
+    def setBlogPost(self):
+        return 0
+    
+    def getBlogPost(self):
+        return 0
