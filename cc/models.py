913ee112cf5fef1d4334703b2e84d2212c225c69
ddiben <ddiben@gmail.com>
2018-01-02 09:14:20 -0800

    set up the first test, added to_display field to BlogPosts model

--- cc/models.py
+++ cc/models.py
@@ -7,6 +7,15 @@ class BlogPosts(models.Model):
     title = models.CharField(max_length=(300),)
     text = models.TextField()
     publish_date = models.DateField()
+    #if this is true, then this is the BlogPost that is displayed (no two posts will have the same title)
+    to_display = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.title
+
+    """ I am thinking right now that I will have it be a list of posts and I am thinking that she will 
+    just select which one she wants to show [granted, she would never show an older post, so maybe I 
+    don't even have to save it] """
     
 class Links(models.Model):
     title = models.CharField(max_length=(300),)
--- cc/tests.py
+++ cc/tests.py
@@ -1,14 +1,15 @@
 from django.test import TestCase
-from .models import BlogPosts, Links
+from .models import BlogPosts
+from datetime import date
+
+
+#testing models
 
 class BlogPostsModelTest(TestCase):
     def test_create_post(self):
-        #create a blogpost
+        post = self.createBlogPost()
         
-        self.assertEquals(self.getblogPost(), 'First blog post')
+        self.assertEquals(post.__str__(), 'Title: 1')
     
-    def setBlogPost(self):
-        return 0
-    
-    def getBlogPost(self):
-        return 0
+    def createBlogPost(self):
+        return BlogPosts(title="Title: 1", text="this is the body", publish_date = date(1994, 5, 9))
